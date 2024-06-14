be4576f956c63329e37f21f2be669f505f9f7792
junreytejas <junreytejas@gmail.com>
2024-06-15 05:03:38 +0800

    Update page.js
    changed approach
    code clean up

--- page.js
+++ page.js
@@ -1,164 +1,145 @@
 const puppeteer = require('puppeteer');
 
-const numRegisters = 16;
-let registers = new Array(numRegisters).fill(0);
-
-
- async function fetchMessages (){
-
-  const encodedMessages = [];
-
-  const browser = await puppeteer.launch({ headless: false });
+async function fetchMessages() {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ['--disable-gpu', '--disable-dev-shm-usage', '--no-sandbox']
+  });
 
   const page = await browser.newPage();
 
+  await page.setRequestInterception(true);
+  page.on('request', (req) => {
+    if (req.resourceType() === 'image' || req.resourceType() === 'stylesheet') {
+      req.abort();
+    } else {
+      req.continue();
+    }
+  });
+
+  console.log('Starting Phase 1');
+  
   await page.goto('https://challenge.longshotsystems.co.uk/go');
 
-  // gather the answers for phase 1
   const htmlDom = await page.evaluate(() => {
     return document.querySelector('.number-panel').textContent.trim().replace(/\s+/g, '');
   });
 
-    // assign name
-    await page.$eval("#name", element => element.value = "Junrey Tejas");
-
-
-  // assign the numbers
+  await page.$eval("#name", element => element.value = "Junrey Tejas");
   await page.$eval('#answer', (element, text) => {
     element.value = text;
   }, htmlDom);
-
-
-  // call submit()
   await page.$eval(".answer-panel button", buttonElement => buttonElement.click());
 
-  // wait 2 page navigations
   await page.waitForNavigation();
   await page.waitForNavigation();
 
-  // Set up WebSocket listener
-  const client = await page.createCDPSession();
-  await client.send('Network.enable');
+  console.log('Starting Phase 2');
 
-  client.on('Network.webSocketFrameReceived', ({ response }) => {
-    const { payloadData } = response;
-    encodedMessages.push(payloadData);
-  });
+  let ableToFetch = false;
 
-  // Wait for messages
-  await page.evaluate(() => {
-    return new Promise(resolve => {
-      setTimeout(resolve, 2000);
-    });
-  });
-
-
-  // decode the messages from the websocket
-  const decodedMessages = readAndDecodeMessages(encodedMessages);
-
-  // loop through the commands and execute
-  decodedMessages.forEach(command => {
-    executeCommand(command);
-  });
-
-  // Calculate sum of registers
-  const sum = registers.reduce((acc, curr) => {
-    return acc + curr;
-  }, 0);
-
-  // Encode the sum of registers in Base64
-  const encodedBase64 = btoa(sum)
-  console.log('Encoded Base64:', encodedBase64);
-
-  // Send the encodedBase64 to the WebSocket
-  await client.send('Network.webSocketFrameSent', encodedBase64).then((response) => {
-    // Handle the response here
-    console.log('Response received:', response);
-  }).catch((error) => {
-    // Handle any errors that occur during sending or receiving the frame
-    console.error('Error sending WebSocket frame:', error);
-  }).finally(
-    console.log("completed execution.")
-  )
-
-  client.on('Network.webSocketFrameReceived', ({ response }) => {
-
-    console.log('WebSocket message received:', response);
- 
-  });
-
-};
-
-// Function to decode Base64 encoded messages
-function decodeBase64(encodedMessage) {
-  const decodedMessage = atob(encodedMessage)
-  return decodedMessage;
-}
-
-// Function to read and decode messages from an array
-function readAndDecodeMessages(messages) {
   try {
-    const decodedMessages = messages.map(encodedMessage => {
-      // Remove "ok:10 " prefix and trim if necessary
-      const cleanedMessage = encodedMessage.replace(/^ok:10 /, '').trim();
-      return decodeBase64(cleanedMessage);
+    ableToFetch = await page.evaluate(async () => {
+
+      const numRegisters = 16;
+      let registers = new Array(numRegisters).fill(0);
+
+      return await new Promise((resolve, reject) => {
+
+        window.ws.onmessage = function (event) {
+
+          const decodedMessage = atob(event.data).trim()
+
+          console.log(decodedMessage)
+
+          if (decodedMessage.toString() == "\"END\"") {
+            console.log("processing end message.")
+            const sum = registers.reduce((acc, curr) => acc + curr, 0);
+
+            const encodedBase64 = btoa(sum.toString());
+
+            console.log('Encoded Base64:', encodedBase64, sum);
+
+            window.ws.send(encodedBase64)
+          } else {
+            console.log("executing command")
+            executeCommand(decodedMessage, registers)
+          }
+
+          resolve(true);
+
+        };
+
+        window.ws.onclose = function (CloseEvent) {
+          console.log(CloseEvent);
+          reject('WebSocket closed. Code: ' + CloseEvent.code);
+        };
+
+      });
+
+      function executeCommand(command, registers) {
+        const parts = command.replace(/"/g, '').split(' ');
+        const opcode = parts[0];
+
+        switch (opcode) {
+          case 'ADD':
+            addCommand(parts, registers);
+            break;
+          case 'MOV':
+            moveCommand(parts, registers);
+            break;
+          case 'STORE':
+            storeCommand(parts, registers);
+            break;
+          default:
+            // console.error(`Unknown instruction: ${command}`);
+        }
+      }
+
+      function addCommand(parts, registers) {
+        const value = parseInt(parts[1]); // Extract the value
+        const srcReg = parseInt(parts[2].substring(1)); // Extract source number
+        const destReg = parseInt(parts[3].substring(1)); // Extract destination number
+
+        registers[destReg] = registers[srcReg] + value;
+      }
+
+      function moveCommand(parts, registers) {
+        const srcReg = parseInt(parts[1].substring(1)); // Extract source number
+        const destReg = parseInt(parts[2].substring(1)); // Extract destination number
+
+        registers[destReg] = registers[srcReg];
+      }
+
+      function storeCommand(parts, registers) {
+        const value = parseInt(parts[1]);
+        const destReg = parseInt(parts[2].substring(1)); // Extract destination number to override
+
+        registers[destReg] = value;
+      }
+
     });
-    return decodedMessages;
-  } catch (err) {
-    console.error('Error decoding messages:', err);
-    return [];
-  }
-}
-
-// break down the instructions and decipher the operation
-function executeCommand(command) {
-  const parts = command.replace(/"/g, '').split(' ');
-  const opcode = parts[0];
-
-  switch (opcode) {
-    case 'ADD':
-      addCommand(parts);
-      break;
-    case 'MOV':
-      moveCommand(parts);
-      break;
-    case 'STORE':
-      storeCommand(parts);
-      break;
-    default:
-      // for instructions that are not in correct format
-      console.error(`Unknown instruction: ${command}`);
+  } catch (error) {
+    console.error('Error:', error);
   }
 
-  // log the updated results after the operation for tracking
-  console.log({instruction: command},{registers})
+  if (!ableToFetch) {
+    // sometimes the websocket fails to open mainly due to network situation. In this case, we retry
+    await browser.close();
+    fetchMessages();
+  }
 
 }
 
-function addCommand(parts) {
-  const value = parseInt(parts[1]); // extract the value
-  const srcReg = parseInt(parts[2].substring(1)); // Extract source number
-  const destReg = parseInt(parts[3].substring(1)); // Extract destination number
-
-  registers[destReg] = registers[srcReg] + value;
-}
+fetchMessages().catch(error => {
+  console.error('Error:', error);
+});
 
 
-function moveCommand(parts) {
-  const srcReg = parseInt(parts[1].substring(1)); // Extract source number
-  const destReg = parseInt(parts[2].substring(1)); // Extract destination number
 
-  registers[destReg] = registers[srcReg];
-}
-
-
-function storeCommand(parts) {
-  const value = parseInt(parts[1]);
-  const destReg = parseInt(parts[2].substring(1)); // Extract destination number to overide
-
-  registers[destReg] = value;
-}
-
-
-// start the program
-fetchMessages()
+/**
+ * final output was:
+ * 
+ * Correct! Send the following hash along with your name to Longshot: 
+ * d50749a1866f88f9cff2bdd137b845170f9623f7645a63efcc55728f63f658ad
+ */
